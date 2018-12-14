dca939053e49ae68c3347217a5d06bebd2bf0b57
telgerot <edward.k.robe@gmail.com>
2018-12-14 09:15:10 +0200

    Added colors to game objects, background, and set up an obstacle course with rotating obstacle

---
+++ Assets/Scripts/RotatingObstacle.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingObstacle : MonoBehaviour {
+
+    [SerializeField] float rotationSpeed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        Rotate();
+	}
+
+    void Rotate()
+    {
+        float rotationThisFrame = rotationSpeed * Time.deltaTime;
+        transform.Rotate(Vector3.forward * rotationThisFrame);
+    }
+}
