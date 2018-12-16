5180ed8b7025403e28f37b9beee8de54dadfb46a
telgerot <edward.k.robe@gmail.com>
2018-12-16 15:16:47 +0200

    Added automatic oscillation (using sin and tau and other magical things)

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -4,7 +4,9 @@ using UnityEngine;
 
 public class Oscillator : MonoBehaviour {
 
-    [SerializeField] Vector3 movementVector;
+    [SerializeField] Vector3 movementVector = new Vector3(10f, 10f, 10f);
+    [SerializeField] float period;
+
     [SerializeField][Range(0,1)]float movementFactor;
 
     Vector3 startingPos;
@@ -14,8 +16,21 @@ public class Oscillator : MonoBehaviour {
 	}
 	
 	void Update () {
-        Vector3 offset = movementVector * movementFactor;
-        transform.position = startingPos + offset;
+        float cycles = Time.time / period;
+
+        if (period <= Mathf.Epsilon)
+        {
+            return;
+        }
+        else
+        {
+            const float tau = Mathf.PI * 2;
+            float rawSinWave = Mathf.Sin(cycles * tau);
+
+            movementFactor = (rawSinWave / 2f) + 0.5f;
+            Vector3 offset = movementVector * movementFactor;
+            transform.position = startingPos + offset;
+        }
         
 	}
 }
