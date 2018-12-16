c8267bf0e71a539d89059ecf3e540dd68b9e134f
telgerot <edward.k.robe@gmail.com>
2018-12-16 12:27:54 +0200

    Added 3rd level, manually oscillating obstacle

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour {
+
+    [SerializeField] Vector3 movementVector;
+    [SerializeField][Range(0,1)]float movementFactor;
+
+    Vector3 startingPos;
+
+	void Start () {
+        startingPos = transform.position;
+	}
+	
+	void Update () {
+        Vector3 offset = movementVector * movementFactor;
+        transform.position = startingPos + offset;
+        
+	}
+}
